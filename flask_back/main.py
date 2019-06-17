4827fc83501f6e83051a120b93f9a61760a771ce
Jack Bruce <jackbruce0@gmail.com>
2019-06-17 16:59:02 -0700

    Basic functioning database
    22 mins into vid

--- flask_back/main.py
+++ flask_back/main.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 import flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = flask.Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/unclesam/webapp_react/flask_back/db/todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/unclesam/HAVIC_XSS_2/flask_back/db/todo.db'
 
 db = SQLAlchemy(app)
 # Example for creating a db of User's and their todo tasks
 class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(80))
+    password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
 
 class Todo(db.Model):
@@ -23,19 +27,75 @@ class Todo(db.Model):
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
-    return ''
+    users = User.query.all()
 
-@app.route('/user/<user_id>', methods=['PUT'])
-def promote_user():
-    return ''
+    output = []
 
-@app.route('/user/<user_id>', methods=['PUT'])
-def promote_user():
-    return ''
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
 
-@app.route('/user/<user_id>', methods=['DELETE'])
-def delete_user():
-    return ''
+    return jsonify({'users' : output})
+
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+    
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+    
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+
+    return jsonify({'user' : user_data}) 
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], 
+            password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'New user created!'})
+
+@app.route('/user/<public_id>', methods=['PUT'])
+def promote_user(public_id):
+    
+    user = User.query.filter_by(public_id=public_id).first()
+    
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+ 
+    user.admin = True
+    db.session.commit()
+    
+
+    return jsonify({'message' : 'The user has been promoted!'})
+
+@app.route('/user/<public_id>', methods=['DELETE'])
+def delete_user(public_id):
+    
+    user = User.query.filter_by(public_id=public_id).first()
+    
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+ 
+    db.session.delete(user)
+    db.session.commit()
+    
+    return jsonify({'message' : 'The user has been deleted!'})
+
+#@app.route('/login')
 
 @app.route("/")
 def my_index():
